742fb84a166143a3b7d3bb555ce21d79d7f04aef
Marc Piñol <marc.pinol@hexagon.com>
2022-03-28 23:42:02 +0200

    Add project files.

---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DeskBooker.Core.Domain;
+using DeskBooker.Core.Processor;
+using Xunit;
+
+namespace DeskBooker.Core.Tests.Processor
+{
+    public class DeskBookingRequestProcessorTest
+    {
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            //Arrange
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Marc",
+                LastName = "Piñol",
+                Email = "marc.pinol@test.com",
+                Date = new DateTime(2022, 03, 29)
+            };
+            
+            var processor = new DeskBookingRequestProcessor();
+
+            //Act
+            DeskBookingResult result = processor.BookDesk(request);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FirstName, result.FirstName);
+            Assert.Equal(request.LastName, result.LastName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+        }
+    }
+}
