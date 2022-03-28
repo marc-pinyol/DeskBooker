2c98d93ac4435355431ae8c1e521c92a93dfc06e
Marc Piñol <marc.pinol@hexagon.com>
2022-03-29 00:15:10 +0200

    Code refacoring after added save logic

--- DeskBooker.Core/Domain/DeskBooking.cs
+++ DeskBooker.Core/Domain/DeskBooking.cs
@@ -1,9 +1,6 @@
 ﻿namespace DeskBooker.Core.Domain;
 
-public class DeskBooking
+public class DeskBooking : DeskBookingBase
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public DateTime Date { get; set; }
+    
 }
---
+++ DeskBooker.Core/Domain/DeskBookingBase.cs
@@ -0,0 +1,9 @@
+﻿namespace DeskBooker.Core.Domain;
+
+public class DeskBookingBase
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public DateTime Date { get; set; }
+}
