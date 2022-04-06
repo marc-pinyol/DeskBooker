4f8cb784f8ff08990a415bc0b8b5a4ce81bf2f1f
Marc Piñol <marc.pinol@hexagon.com>
2022-04-06 11:26:47 +0200

    Added validations to DeskBooking properties

---
+++ DeskBooker.Core.Tests/Validation/DateWithoutTimeAttributeTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeskBooker.Core.Validation;
+using Xunit;
+
+namespace DeskBooker.Core.Tests.Validation
+{
+    public class DateWithoutTimeAttributeTests
+    {
+        [InlineData(true, 0, 0, 0)]
+        [InlineData(false, 1, 0, 0)]
+        [InlineData(false, 0, 1, 0)]
+        [InlineData(false, 0, 0, 1)]
+        [Theory]
+        public void ShouldReturnDateMustBeInTheFuture(bool expectedIsValid, int hour, int minute, int second)
+        {
+            var dateTime = new DateTime(2020, 1, 28, hour, minute, second);
+
+            var attribute = new DateWithoutTimeAttribute();
+
+            var isValid = attribute.IsValid(dateTime);
+
+            Assert.Equal(expectedIsValid, isValid);
+        }
+
+        [Fact]
+        public void ShouldHaveExpectedErrorMessage()
+        {
+            var attribute = new DateWithoutTimeAttribute();
+
+            Assert.Equal("Date must not contain time", attribute.ErrorMessage);
+        }
+    }
+}
--- DeskBooker.Core/Domain/DeskBookingBase.cs
+++ DeskBooker.Core/Domain/DeskBookingBase.cs
@@ -1,9 +1,22 @@
-﻿namespace DeskBooker.Core.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeskBooker.Core.Domain;
 
 public class DeskBookingBase
 {
+    [Required]
+    [StringLength(50)]
     public string FirstName { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public string LastName { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    [EmailAddress]
     public string Email { get; set; }
+    
+    [DataType(DataType.Date)]
     public DateTime Date { get; set; }
 }
